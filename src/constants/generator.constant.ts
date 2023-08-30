3717d3de6b8f932df3f50884a651b2709f92c6d1
Nicolas Santuccio <nico.santuccio@gmail.com>
2023-08-29 22:30:37 -0300

    feat: add generators options

---
+++ src/constants/generator.constant.ts
@@ -0,0 +1,10 @@
+import type { Generator } from 'src/types/generator.type.js'
+
+export const GENERATORS_OPTIONS: Record<Generator, string> = {
+  'github-actions': 'Github Actions',
+  husky: 'Huksy',
+  'next-ts': 'Next.js Typescript',
+  'node-ts': 'Node.js Tyepscript',
+  'react-ts': 'React.js Typescript',
+  vscode: 'Visual Studio Code',
+}
