1123f80ed9c2ea09bf09dc700dc1b5adf20ec5d4
Nicolas Santuccio <nico.santuccio@gmail.com>
2023-09-01 17:16:21 -0300

    feat: add react dependencies array

---
+++ src/dependencies/index.ts
@@ -0,0 +1,16 @@
+export const nextDependencies = ['']
+
+export const nodeDependencies = ['']
+
+export const reactDependencies = [
+  '@typescript-eslint/eslint-plugin',
+  '@typescript-eslint/parser',
+  'eslint',
+  'eslint-config-prettier',
+  'eslint-plugin-import',
+  'eslint-plugin-node',
+  'eslint-plugin-promise',
+  'eslint-plugin-react',
+  'eslint-plugin-react-hooks',
+  'prettier',
+]
