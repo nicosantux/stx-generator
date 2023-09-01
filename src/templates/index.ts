aa1e6c0ef508084e632e522f2cc950cd75c5d2aa
Nicolas Santuccio <nico.santuccio@gmail.com>
2023-09-01 16:42:29 -0300

    feat: add prettier, eslint, and editorconfig templates

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,1 +1,4 @@
+export * from './editorconfig.template.js'
+export * from './eslint.template.js'
+export * from './prettier.template.js'
 export * from './vscode.template.js'
---
+++ src/templates/prettier.template.ts
@@ -0,0 +1,20 @@
+export const prettierIgnore = `dist
+build
+package-lock.json
+pnpm-lock.yaml
+yarn.lock`
+
+export const prettierrc = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
