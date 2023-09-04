c229a2daf6ed39ed71d7323cc5a5a3540d6b4a6e
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-03 23:21:19 -0300

    chore: update generator type in add generators files

--- src/utils/add-generator-files.util.ts
+++ src/utils/add-generator-files.util.ts
@@ -1,4 +1,4 @@
-import type { GeneratorFiles } from '../types/generator.type.js'
+import type { Generator } from '../types/generator.type.js'
 
 import fs from 'node:fs/promises'
 import path from 'node:path'
@@ -7,7 +7,7 @@ import { GENERATORS_FILES } from '../constants/generator.constant.js'
 
 import { writeJSON } from './json.util.js'
 
-export const addGeneratorFiles = async (generator: GeneratorFiles, tailwind: boolean = false) => {
+export const addGeneratorFiles = async (generator: Generator, tailwind: boolean = false) => {
   const files = GENERATORS_FILES[generator]
 
   Object.entries(files).map(([filename, { destination, file, json }]) => {
