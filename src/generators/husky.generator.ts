158900e49ff06fd29c0f76fe84ba10dc7566a8a9
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-03 23:24:22 -0300

    feat: add husky generator

--- src/generators/husky.generator.ts
+++ src/generators/husky.generator.ts
@@ -1,4 +1,67 @@
+import type { Option, PackageManger } from '../types/index.js'
+
+import { confirm, select, spinner as createSpinner, outro } from '@clack/prompts'
+import pc from 'picocolors'
+
+import {
+  addGeneratorFiles,
+  execCmd,
+  getProjectPackageManager,
+  handleCancelPrompt,
+  installDependencies,
+} from '../utils/index.js'
+import { PACKAGE_MANAGER } from '../constants/index.js'
+
 export const husky = async () => {
-  // TODO IMPLEMENT
-  console.log('Husky')
+  try {
+    await execCmd('git status')
+  } catch (_) {
+    const initializeGit = handleCancelPrompt(
+      await confirm({
+        message: 'You are not in a git repository. Do you want to initialize git?',
+        initialValue: true,
+      }),
+    )
+
+    if (!initializeGit) {
+      outro('Santux generator canceled')
+
+      process.exit(0)
+    }
+
+    await execCmd('git init')
+  }
+
+  let packageManager = await getProjectPackageManager()
+
+  if (!packageManager) {
+    packageManager = handleCancelPrompt(
+      await select<Option<PackageManger>[], PackageManger>({
+        message: 'Which package manager does your project use?',
+        options: PACKAGE_MANAGER.map((value) => ({ label: value, value })),
+      }),
+    )
+  }
+
+  addGeneratorFiles('husky')
+
+  await installDependencies({ generator: 'husky', packageManager })
+
+  const spinner = createSpinner()
+
+  spinner.start(`Installing git hooks`)
+
+  await execCmd('npx husky install')
+
+  await execCmd('npm pkg set scripts.prepare="husky install"')
+
+  await execCmd('npm pkg set scripts.commitlint="commitlint --edit"')
+
+  const cmd = 'npx husky add .husky/commit-msg "pkg run commitlint \\${1}"'
+
+  await execCmd(cmd.replace('pkg', packageManager))
+
+  spinner.stop(`Git hooks installed`)
+
+  outro(pc.bgCyan(pc.black(' Added commit-msg git hook and .commitlintrc.json ')))
 }
