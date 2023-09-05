e9d8ddf9579e8c1799aa8da680433f8d6e4fa252
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-05 20:43:58 -0300

    chore: update prettier format scripts

--- src/utils/add-lint-and-format-scripts.util.ts
+++ src/utils/add-lint-and-format-scripts.util.ts
@@ -3,5 +3,6 @@ import { execCmd } from './index.js'
 export const addLintAndFormatScripts = async () => {
   await execCmd('npm pkg set scripts.lint="eslint ."')
   await execCmd('npm pkg set scripts.lint:fix="eslint . --fix --ext .tsx,.ts,.jsx,.js,.cjs,.mjs"')
-  await execCmd('npm pkg set scripts.format="prettier --write ."')
+  await execCmd('npm pkg set scripts.format="prettier . --check"')
+  await execCmd('npm pkg set scripts.format:fix="prettier . --write"')
 }
