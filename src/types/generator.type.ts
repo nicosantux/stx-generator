41b3781e000f9ab73055d8b5593862f78dce2114
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-09 21:12:51 -0300

    refactor: change utility type

--- src/types/generator.type.ts
+++ src/types/generator.type.ts
@@ -1,3 +1,3 @@
 export type Generator = 'github-actions' | 'husky' | 'next-ts' | 'node-ts' | 'react-ts' | 'vscode'
 
-export type GeneratorLintDeps = Extract<Generator, 'husky' | 'next-ts' | 'node-ts' | 'react-ts'>
+export type GeneratorLintDeps = Exclude<Generator, 'github-actions' | 'vscode'>
