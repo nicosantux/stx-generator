091fc9c7efeeeeee0b15181e4c766d1b095c6834
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-09 21:37:58 -0300

    feat: add github actions generator

--- src/generators/github-actions.generator.ts
+++ src/generators/github-actions.generator.ts
@@ -1,4 +1,27 @@
+import type { GithubWorkflow, Option } from '../types/index.js'
+
+import fs from 'node:fs/promises'
+
+import { multiselect } from '@clack/prompts'
+
+import { addGithubWorkflow, handleCancelPrompt } from '../utils/index.js'
+import { GITHUB_WORKFLOWS } from '../constants/index.js'
+
 export const githubActions = async () => {
-  // TODO IMPLEMENT
-  console.log('Github Actions')
+  const ghActions = handleCancelPrompt(
+    await multiselect<Option<GithubWorkflow>[], GithubWorkflow>({
+      message: 'Select the workflows you want to add',
+      options: Object.entries(GITHUB_WORKFLOWS).map(
+        ([value, label]) => ({ label, value }) as { label: string; value: GithubWorkflow },
+      ),
+      required: true,
+    }),
+  )
+
+  await fs.mkdir('.github', { recursive: true })
+  await fs.mkdir('.github/workflows', { recursive: true })
+
+  for (const workflow of ghActions) {
+    await addGithubWorkflow(workflow)
+  }
 }
---
+++ src/utils/github-actions.util.ts
@@ -0,0 +1,52 @@
+import type { GithubWorkflow } from '../types/index.js'
+
+import { text } from '@clack/prompts'
+
+import { bumpVersion, createRelease, deployment } from '../templates/index.js'
+
+import { handleCancelPrompt } from './handle-cancel-prompt.util.js'
+import { addFile } from './add-file.util.js'
+
+export const addGithubWorkflow = async (workflow: GithubWorkflow) => {
+  if (workflow === 'bump-version') return await addBumpVersionWorkflow()
+  if (workflow === 'create-release') return await addCreateReleaseWorkflow()
+  if (workflow === 'deployment') return await addDeploymentWorkflow()
+}
+
+const addBumpVersionWorkflow = async () => {
+  const username = handleCancelPrompt(
+    await text({
+      message: 'Enter your git username:',
+      placeholder: 'John Doe',
+      validate(value) {
+        if (!value.trim().length) return 'Username is required'
+        if (value.trim().length < 3) return 'Username must have at least 3 characters'
+      },
+    }),
+  )
+
+  const email = handleCancelPrompt(
+    await text({
+      message: 'Enter your git email:',
+      placeholder: 'johndoe@gmail.com',
+      validate(value) {
+        const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        if (!emailRegexp.test(value)) return 'Enter a valid email'
+      },
+    }),
+  )
+
+  await addFile(
+    '.github/workflows/bump-version.yml',
+    bumpVersion.replace('GIT_USERNAME', username).replace('GIT_EMAIL', email),
+  )
+}
+
+const addCreateReleaseWorkflow = async () => {
+  await addFile('.github/workflows/create-release.yml', createRelease)
+}
+
+const addDeploymentWorkflow = async () => {
+  await addFile('.github/workflows/deployment.yml', deployment)
+}
