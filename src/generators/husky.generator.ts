1d727083516118bc2728b22eb7c32eb0e66d48bb
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-02-09 20:59:41 -0300

    fix: update husky generator to v9

--- src/generators/husky.generator.ts
+++ src/generators/husky.generator.ts
@@ -1,8 +1,10 @@
 import type { Option, PackageManger } from '../types/index.js'
 
-import { confirm, select, spinner as createSpinner, outro } from '@clack/prompts'
-import pc from 'picocolors'
+import { confirm, spinner as createSpinner, outro, select } from '@clack/prompts'
+import colors from 'picocolors'
 
+import { PACKAGE_MANAGER } from '../constants/index.js'
+import { huskyDependencies } from '../dependencies/index.js'
 import { commitlint } from '../templates/commitlint.template.js'
 import {
   addFile,
@@ -11,8 +13,6 @@ import {
   handleCancelPrompt,
   installDependencies,
 } from '../utils/index.js'
-import { PACKAGE_MANAGER } from '../constants/index.js'
-import { huskyDependencies } from '../dependencies/index.js'
 
 export const husky = async () => {
   try {
@@ -53,17 +53,13 @@ export const husky = async () => {
 
   spinner.start(`Installing git hooks`)
 
-  await execCmd('npx husky install')
-
-  await execCmd('npm pkg set scripts.prepare="husky install"')
+  await execCmd('npx husky init')
 
   await execCmd('npm pkg set scripts.commitlint="commitlint --edit"')
 
-  const cmd = "npx husky add .husky/commit-msg 'pkg run commitlint ${1}'"
-
-  await execCmd(cmd.replace('pkg', packageManager))
+  await addFile('.husky/commit-msg', `${packageManager} run commitlint \${1}`)
 
   spinner.stop(`Git hooks installed`)
 
-  outro(pc.bgCyan(pc.black(' Added commit-msg git hook and .commitlintrc.json ')))
+  outro(colors.bgCyan(colors.black(' Added commit-msg git hook and .commitlintrc.json ')))
 }
