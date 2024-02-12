3c9ff795f832b14593dd789a07ee3052cd6a57da
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-02-12 10:29:17 -0300

    refactor: make `GITHUB_WORKFLOWS` readonly

--- src/constants/github-action.constant.ts
+++ src/constants/github-action.constant.ts
@@ -1,6 +1,6 @@
 import type { GithubWorkflow } from '../types/index.js'
 
-export const GITHUB_WORKFLOWS: Record<GithubWorkflow, string> = {
+export const GITHUB_WORKFLOWS: Readonly<Record<GithubWorkflow, string>> = {
   'bump-version': 'Bump version',
   'create-release': 'Create release',
   deployment: 'Deployment',
